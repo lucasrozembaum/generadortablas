eb3fc3ffadc19f185bd0e68fb01a26acc65d7c25
LucasRozembaum <lucasrozembaum@gmail.com>
2026-08-24 16:42:45 -0300

    uultimo

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = "uatre-escala-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js",
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        }),
+      ),
+    ),
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
