53083917f4e200d52549a54f3c18aae151a3386f
LucasRozembaum <lucasrozembaum@gmail.com>
2026-08-24 16:59:54 -0300

    incon

--- sw.js
+++ sw.js
@@ -3,6 +3,8 @@ const ASSETS = [
   "./",
   "./index.html",
   "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
   "https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js",
 ];
 
